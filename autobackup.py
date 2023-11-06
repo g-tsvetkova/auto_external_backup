5305d23f4e4313d32f6c70c25de3067c109edc49
g-tsvetkova <97783831+g-tsvetkova@users.noreply.github.com>
2023-11-06 20:29:26 -0300

    Added utils features in autobackup.py

--- autobackup.py
+++ autobackup.py
@@ -1,13 +1,22 @@
 #! python3
-# File: autobackup.py
+# autobackup.py
 
 import dirsync
 import os
+from utils import estimate_backup_size, visualize_backup_statistics
 
 src = "Path_To_Folder_Being_Backedup"
 dest = "Path_To_External_Drive"
 
 if os.path.exists(dest):
+    # Calculate the estimated backup size
+    estimated_size = estimate_backup_size(src)
+    print(f"Estimated backup size: {estimated_size} bytes")
+
+    # Synchronize the directories
     dirsync.sync(src, dest, 'sync', purge=True)
+
+    # Visualize backup statistics
+    visualize_backup_statistics(src, dest)
 else:
     print("Backup storage not found. Exiting...")
